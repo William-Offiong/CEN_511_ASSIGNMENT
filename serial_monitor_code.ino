daa4e7c88c0e2db22f6e7a90223e34b1e83ec048
William Offiong <78592805+William-Offiong@users.noreply.github.com>
2021-02-05 12:00:54 +0100

    Add files via upload

---
+++ serial_monitor_code.ino
@@ -0,0 +1,11 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println("Hello My name is OFFIONG WILLIAM, 16CJ020747");
+  delay(3000);
+}
